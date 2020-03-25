f93f5435a631f54e2106e84747ee975eafd24bfe
Reachea <reacheasambath@gmail.com>
2020-03-25 14:12:16 +0700

    micropost

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_19_131153) do
+ActiveRecord::Schema.define(version: 2020_03_24_141637) do
+
+  create_table "microposts", force: :cascade do |t|
+    t.text "content"
+    t.integer "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
